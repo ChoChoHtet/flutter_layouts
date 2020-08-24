aa2d934df9e08c2a296be53c484af931f8dfcf44
ChoHtet <chocho.htet@haulio.io>
2020-08-24 20:46:10 +0630

    grid view

--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,25 @@ class MyApp extends StatelessWidget {
             children: [_buildImageRow(1), _buildImageRow(3)],
           ),
         );
-
+    List<Container> _buildGridTile(int count) => List.generate(count,
+        (i) =>Container(
+          child: Image.asset('assets/images/love_$i.jpg')
+        )
+       );
+    Widget _buildGrid() => GridView.extent(maxCrossAxisExtent: 200,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 20,
+      crossAxisSpacing: 4,
+      children: _buildGridTile(6),
+    );
     return MaterialApp(
       title: 'Flutter Layouts Demo',
       home: Scaffold(
           appBar: AppBar(
             title: Text('Flutter Layouts Demo'),
           ),
-          body: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // imagesList,leftColumn
-                _buildImageColumn()
-              ],
-            ),
-          )),
+          body: _buildGrid(),
+      )
     );
   }
 }
